6007844d6c29b5ba87ca58241a60e891a73b2ecd
firsovroman <romanfirs@list.ru>
2022-01-14 10:38:40 +0800

    .

---
+++ src/main/java/semaphore_example/IncThread.java
@@ -0,0 +1,38 @@
+package semaphore_example;
+
+import java.util.concurrent.Semaphore;
+
+public class IncThread implements Runnable{
+
+    String name;
+    Semaphore sem;
+
+    public IncThread(Semaphore sem, String a) {
+        this.sem = sem;
+        this.name = a;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Запуск потока: "+ name);
+
+        try {
+            System.out.println("Поток " + name + " ожидает разрешения");
+            sem.acquire();
+            System.out.println("Поток " + name + " получает разрешение");
+
+            for(int  i =0; i<5; i++) {
+                Shared.count++;
+                System.out.println(name + ": " + Shared.count);
+
+                Thread.sleep(10);
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Поток " + name + " освобождает разрешение");
+        sem.release();
+    }
+}
---
+++ src/main/java/semaphore_example/Shared.java
@@ -0,0 +1,7 @@
+package semaphore_example;
+
+public class Shared {
+
+    static int count = 0;
+
+}
