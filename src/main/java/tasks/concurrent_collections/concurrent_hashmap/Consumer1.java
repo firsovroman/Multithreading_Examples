b62ecda11dd6a9e8de59fa5d3f15fd9842b9f4be
Roman <romanfirs@list.ru>
2022-07-20 09:57:45 +0800

    .

---
+++ src/main/java/tasks/concurrent_collections/concurrent_hashmap/Consumer1.java
@@ -0,0 +1,22 @@
+package tasks.concurrent_collections.concurrent_hashmap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Consumer1 implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Consumer1(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+        while (!currentThread.isInterrupted()) {
+            if (!map.isEmpty()) {
+                for (String key : map.keySet()) {
+                    System.out.println(map.remove(key));
+                }
+            }
+        }
+    }
+}
