a87f973e8c1a37c75feebb25b1ee3223e1f72d3f
Roman <romanfirs@list.ru>
2022-07-29 09:41:27 +0800

    .

---
+++ src/test/java/codewars/DinglemouseTest.java
@@ -0,0 +1,14 @@
+//package codewars;
+//
+//import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+//
+//class DinglemouseTest {
+//
+//    @Test
+//    public void test() {
+//        Dinglemouse.runRunners();
+//        assertTrue("Sorry, try again :-(", ThreadUtil.checkThreads());
+//    }
+//
+//}
