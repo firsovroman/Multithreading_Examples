65111995ac440893ed87cc5df336f688afb5fd45
Roman Firsov <firsov_r@stdpr.ru>
2023-08-14 15:01:18 +0300

    .

--- src/main/java/a_primitives/exceptionHandler/Solution.java
+++ src/main/java/a_primitives/exceptionHandler/Solution.java
@@ -1,40 +1,40 @@
-package a_primitives.threadstates;
+package a_primitives.exceptionHandler;
 
+/*
+    В примере показано как у потока можно переопределить перехватчик исключений.
+
+ */
 public class Solution {
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread myThread = new Solution().new MyThread("super secret key");
+        MyThread myThread = new Solution().new MyThread();
         myThread.start();
         Thread.sleep(3000);
     }
 
     public class MyThread extends Thread {
-        private String secretKey;
 
-        public MyThread(String secretKey) {
-            this.secretKey = secretKey;
+        public MyThread() {
             setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
             setDaemon(false);
         }
 
         @Override
         public void run() {
-            throw new NullPointerException("it's an example");
+            throw new NullPointerException("Exception message");
         }
 
         private class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
             @Override
             public void uncaughtException(Thread t, Throwable e) {
-
-
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
 
-                String s = String.format("%s, %s, %s", secretKey, t.getName(), e.getMessage());
+                String s = String.format("Exception with message %s, in  %s, was suppressed.", e.getMessage(), t.getName());
 
                 System.out.println(s);
             }
